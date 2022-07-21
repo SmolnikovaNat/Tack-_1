a9ce78732be2d8ba7db2770550281346b44d30fa
Natalya <h.h.smolnikova@mail.ru>
2022-07-21 20:44:59 +0500

    задача 27

---
+++ ex27/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+            int x = int.Parse(Console.ReadLine());
+            int i;
+            for (i = x == 0 ? 0 : 1; x > 0; x /= 10)
+            {
+                i += x % 10;
+            }
+            Console.WriteLine(i-1);
+        }
+    }
+}
